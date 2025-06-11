60df84521e261c4104fab6ea069f4caaf833a0fa
poki95 <112725946+poki95@users.noreply.github.com>
2025-06-11 19:26:45 +0200

    bot.py -> 2nd version

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import math
@@ -8,8 +9,9 @@ import roman
 import csv
 
 # Discord bot token and hypixel api key
-TOKEN = ''
-api_key = ""						# update every 3 days 
+load_dotenv(dotenv_path="keys.env")
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+api_key = os.getenv("HYPIXEL_API_KEY")									# update every 3 days 
 
 # Discord bot setup
 intents = discord.Intents.default()
@@ -24,28 +26,26 @@ div_list = ['ASCENDED', 'DIVINE', 'CELESTIAL', 'Godlike', 'Grandmaster', 'Legend
 div_req = [100000, 50000, 25000, 10000, 5000, 2000, 1000, 500, 250, 100, 50, 0] # requirements for each division title
 div_step = [10000, 10000, 5000, 3000, 1000, 600, 200, 100, 50, 30, 10, 1] # requirements to go up a level within a division title
 
+
+
 # Functions
-def dataget(ign):															# Get player's UUID and load player's Hypixel stats
+def dataget(ign):														# Load player's Hypixel stats
 	error = True
-	while error == True:												# ask for the player's ign whose stats are going to be checked
+	while error == True:												# get player's uuid to avoid 5 min cooldown
 		try:															
 			response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{ign}")
 			uuid = response.json()["id"]
 			error = False
 		except KeyError:
 			error = True
-			data(ign)
-	data = requests.get(												# load the player's stats
-		url = "https://api.hypixel.net/player",
-		params = {
-			"key": api_key,
-			"uuid": uuid
-		}
-	)
+			('Invalid IGN. Try again.')
+		url = f"https://api.hypixel.net/player?key={api_key}&uuid={uuid}"
+	res = requests.get(url)
+	data = res.json()
 	return data
 	
 def mode_clean(mode):
-	while mode not in mode_list:											# check that the mode is valid
+	while mode not in mode_list:										# check that the mode is valid
 		'''if mode == 'help':
 			print('Available modes:')
 			for i in mode_list:
@@ -61,11 +61,35 @@ def mode_clean(mode):
 		print("Type 'help' to get the mode aliases.")
 		mode = str(input('Mode: ')).lower()
 		if mode != 'all':		
-			mode_index = mode_list.index(mode)        							# convert the alias to the mode's full name
+			mode_index = mode_list.index(mode)        					# convert the alias to the mode's full name
 			mode_clean = mode_names[mode_index]
 	return mode_clean
 
-
+def get_rank(ign, data):
+    try:
+        data["player"]
+    except KeyError:
+        return (data)
+    if "newPackageRank" in data["player"]:
+        rank = (data["player"]["newPackageRank"]) # A player's rank (excluding MVP++) is stored under"rank" in "newPackageRank".
+        if rank == 'MVP_PLUS':
+            if "monthlyPackageRank" in data["player"]: # This category only exists for users who have bought MVP++ at least once
+                mvp_plus_plus = (data["player"]["monthlyPackageRank"]) # MVP++ is stored separately under "monthlyPackageRank"
+                if mvp_plus_plus == "NONE":
+                    return ('[MVP+]')
+                else:
+                    return("[MVP++]")
+            else:
+                return("[MVP+]")
+        elif rank == 'MVP':
+            return ('[MVP]')
+        elif rank == 'VIP_PLUS':
+            return ('[VIP+]')
+        elif rank == 'VIP':
+            return ('[VIP]')
+    else:
+        return ('')
+        
 # Bot readiness confirmation
 @bot.event
 async def on_ready():
@@ -75,36 +99,60 @@ async def on_ready():
 @bot.command(name='h')
 async def h(ctx):
 	await ctx.send(f"""
-Show this message -> !h
-Check overall duels wins for [ign] -> !d [ign]
-Check Skywars Duels [kit] wins for [ign] -> !kit [ign] [kit]
+# **Help Menu**
+
+- `!d <ign> <mode>` — Check wins and division titles for a player in a specific duels mode.
+  -> `!d Technoblade sw`
+
+- `!d <ign> all` — Check wins and division titles across all duels modes.
+  -> `!d Technoblade all`
+## Soon to come:
+- `!kit <ign> <kit>` — Check (ranked) kit wins in SkyWars duels.
+  -> `!kit Technoblade enderman`
+
+Available modes: `oa`, `uhc`, `sw`, `mw`, `blitz`, `op`, `classic`, `bow`, `ndb`, `combo`, `tnt`, `sumo`, `bridge`, `pkd`, `boxing`, `arena`
+
+For any issues, contact **poki95** on Discord.
 """)
 
 @bot.command(name='d')
 async def d(ctx, ign, mode):
-	data = dataget(ign)
-	if mode == 'all':
+#	data = dataget(ign)
+	url = f"https://api.hypixel.net/player?key={api_key}&name={ign}"
+	res = requests.get(url)
+	data = res.json()
+	try:
+		data["player"]
+	except KeyError:
+		if str(data) == "{'success': False, 'cause': 'You have already looked up this name recently'}":
+			await ctx.send('You have already looked up this name recently. Try again in 5 minutes!')
+		else:
+			await ctx.send('Unidentified error. Contact `poki95` on Discord. Thanks!')
+	displayname = data["player"]["displayname"]
+	if mode not in mode_list:
+		await ctx.send(f'`{mode}` is not an available mode. Type `!h` for help.')
+	elif mode == 'all':
 		for mode_db in mode_db_list:
 			if mode_db == 'oa':
 				try:
-					globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"]["wins"]
+					globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"]["wins"]
 				except KeyError:  
 					globals()[f'{mode_db}_wins'] = 0
 			elif mode_db == 'uhc':
 				try:
-					globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+					globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 				except KeyError:  
 					globals()[f'{mode_db}_duel_wins'] = 0
 				try:
-					globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+					globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_doubles_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_four_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
+					globals()[f'{mode_db}_four_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_four_wins'] = 0
 				try:
-					globals()[f'{mode_db}_meetup_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_meetup_wins"]
+					globals()[f'{mode_db}_meetup_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_meetup_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_meetup_wins'] = 0
 				globals()[f'{mode_db}_wins'] = (
@@ -114,41 +162,41 @@ async def d(ctx, ign, mode):
 				+ globals()[f'{mode_db}_meetup_wins'])
 			elif mode_db in ['sw', 'mw', 'op']:
 				try:
-					globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+					globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 				except KeyError:  
 					globals()[f'{mode_db}_duel_wins'] = 0
 				try:
-					globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+					globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_doubles_wins'] = 0
 				globals()[f'{mode_db}_wins'] = (globals()[f'{mode_db}_duel_wins'] + globals()[f'{mode_db}_doubles_wins'])
 			elif mode_db == 'bridge':
 				try:
-					globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+					globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_duel_wins'] = 0
 				try:
-					globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+					globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_doubles_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_threes_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_threes_wins"]
+					globals()[f'{mode_db}_threes_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_threes_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_threes_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_four_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
+					globals()[f'{mode_db}_four_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_four_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_2v2v2v2_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_2v2v2v2_wins"]
+					globals()[f'{mode_db}_2v2v2v2_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_2v2v2v2_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_2v2v2v2_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_3v3v3v3_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_3v3v3v3_wins"]
+					globals()[f'{mode_db}_3v3v3v3_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_3v3v3v3_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_3v3v3v3_wins'] = 0
 				try:	
-					globals()[f'{mode_db}_capture_threes_wins'] = data.json()["player"]["stats"]["Duels"]["capture_threes_wins"]
+					globals()[f'{mode_db}_capture_threes_wins'] = data["player"]["stats"]["Duels"]["capture_threes_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_capture_threes_wins'] = 0
 				globals()[f'{mode_db}_wins'] = (
@@ -161,20 +209,19 @@ async def d(ctx, ign, mode):
 				+ globals()[f'{mode_db}_capture_threes_wins'])
 			elif mode_db in ['parkour_eight', 'duel_arena']:
 				try:
-					globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_wins"]
+					globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_wins'] = 0
 			else:	
 				try:
-					globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+					globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 				except KeyError:
 					globals()[f'{mode_db}_wins'] = 0
 
 			mode_index = mode_db_list.index(mode_db)        				# convert the database alias to the mode's full name
 			mode_clean = mode_names[mode_index]
 			#print(f'{mode_clean} wins: {globals()[f'{mode_db}_wins']}')	# print the wins for every mode
-		
-		
+				
 		for mode_db in mode_db_list:
 			mode_index = mode_db_list.index(mode_db)        				# convert the database alias to the mode's full name
 			mode_clean = mode_names[mode_index]
@@ -224,27 +271,27 @@ async def d(ctx, ign, mode):
 				else:
 						globals()[f'message_{mode_db}'] = f'{mode_clean} {div} - {wincount} wins'
 		await ctx.send(f'''
-		[MVP++] {ign}
-		{message_oa}
-		{message_uhc}
-		{message_sw}
-		{message_mw}
-		{message_blitz}
-		{message_op}
-		{message_classic}
-		{message_bow}
-		{message_potion}
-		{message_combo}
-		{message_bowspleef}
-		{message_sumo}
-		{message_bridge}
-		{message_parkour_eight}
-		{message_boxing}
-		{message_duel_arena}
-		''')
+{get_rank(ign, data)} {displayname}
+{message_oa}
+{message_uhc}
+{message_sw}
+{message_mw}
+{message_blitz}
+{message_op}
+{message_classic}
+{message_bow}
+{message_potion}
+{message_combo}
+{message_bowspleef}
+{message_sumo}
+{message_bridge}
+{message_parkour_eight}
+{message_boxing}
+{message_duel_arena}
+''')
 						
 #		try:
-#			capture_duel_wins = data.json()["player"]["stats"]["Duels"]["capture_duel_wins"]
+#			capture_duel_wins = data["player"]["stats"]["Duels"]["capture_duel_wins"]
 #		except KeyError:
 #			capture_duel_wins = 0
 #		ghost_wins = (capture_duel_wins)
@@ -254,25 +301,25 @@ async def d(ctx, ign, mode):
 		mode_db = mode_db_list[mode_index]
 		if mode_db == 'oa':
 			try:
-				globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"]["wins"]
+				globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"]["wins"]
 			except KeyError:  
 				globals()[f'{mode_db}_wins'] = 0
 		elif mode_db == 'uhc':
 			
 			try:
-				globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+				globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 			except KeyError:  
 				globals()[f'{mode_db}_duel_wins'] = 0
 			try:
-				globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+				globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_doubles_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_four_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
+				globals()[f'{mode_db}_four_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_four_wins'] = 0
 			try:
-				globals()[f'{mode_db}_meetup_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_meetup_wins"]
+				globals()[f'{mode_db}_meetup_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_meetup_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_meetup_wins'] = 0
 			globals()[f'{mode_db}_wins'] = (
@@ -282,41 +329,41 @@ async def d(ctx, ign, mode):
 			+ globals()[f'{mode_db}_meetup_wins'])
 		elif mode_db in ['sw', 'mw', 'op']:
 			try:
-				globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+				globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 			except KeyError:  
 				globals()[f'{mode_db}_duel_wins'] = 0
 			try:
-				globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+				globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_doubles_wins'] = 0
 			globals()[f'{mode_db}_wins'] = (globals()[f'{mode_db}_duel_wins'] + globals()[f'{mode_db}_doubles_wins'])
 		elif mode_db == 'bridge':
 			try:
-				globals()[f'{mode_db}_duel_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+				globals()[f'{mode_db}_duel_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_duel_wins'] = 0
 			try:
-				globals()[f'{mode_db}_doubles_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
+				globals()[f'{mode_db}_doubles_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_doubles_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_doubles_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_threes_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_threes_wins"]
+				globals()[f'{mode_db}_threes_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_threes_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_threes_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_four_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
+				globals()[f'{mode_db}_four_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_four_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_four_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_2v2v2v2_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_2v2v2v2_wins"]
+				globals()[f'{mode_db}_2v2v2v2_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_2v2v2v2_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_2v2v2v2_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_3v3v3v3_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_3v3v3v3_wins"]
+				globals()[f'{mode_db}_3v3v3v3_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_3v3v3v3_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_3v3v3v3_wins'] = 0
 			try:	
-				globals()[f'{mode_db}_capture_threes_wins'] = data.json()["player"]["stats"]["Duels"]["capture_threes_wins"]
+				globals()[f'{mode_db}_capture_threes_wins'] = data["player"]["stats"]["Duels"]["capture_threes_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_capture_threes_wins'] = 0
 			globals()[f'{mode_db}_wins'] = (
@@ -329,12 +376,12 @@ async def d(ctx, ign, mode):
 			+ globals()[f'{mode_db}_capture_threes_wins'])
 		elif mode_db in ['parkour_eight', 'duel_arena']:
 			try:
-				globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_wins"]
+				globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_wins'] = 0
 		else:	
 			try:
-				globals()[f'{mode_db}_wins'] = data.json()["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
+				globals()[f'{mode_db}_wins'] = data["player"]["stats"]["Duels"][f"{mode_db}_duel_wins"]
 			except KeyError:
 				globals()[f'{mode_db}_wins'] = 0
 		# print(f'{globals()[f'{mode_db}_wins']}')
@@ -388,6 +435,13 @@ async def d(ctx, ign, mode):
 			else:
 					await ctx.send(f'{mode_clean} {div} - {wincount} wins')
 
+@bot.command(name='kit')
+async def kit(ctx, ign, mode):
+	#	data = dataget(ign)
+	url = f"https://api.hypixel.net/player?key={api_key}&name={ign}"
+	res = requests.get(url)
+	data = res.json()
+	
 
 # Run the bot
 bot.run(TOKEN)
