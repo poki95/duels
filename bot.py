67ef246d285f1862857c25a8fb44b513deb909d9
poki95 <112725946+poki95@users.noreply.github.com>
2025-07-19 14:19:14 +0200

    bot.py -> version 8
    - added error messages
    - optimized the code
    - removed kills from pkd, boxing and tnt
    - fixed !d oa not showing the correct estimated playtime
    - fixed bridge kills not showing correctly
    - fixed some !kit variations to show 'Sw' and 'top10' kit wins
    - fixed the help menu
    - reworked !uuid command
    - reworked the !s command
    - reworked the way logs are made

--- bot.py
+++ bot.py
@@ -58,7 +58,8 @@ kits_mw = [
     'shaman', 'hunter', 'creeper', 'automaton', 'enderman', 'blaze',
     'assassin', 'snowman', 'renegade', 'dragon', 'spider', 'sheep', 'skeleton'
 ]
-kits = kits_sw + kits_blitz + kits_mw
+kits = kits_sw + kits_blitz + kits_mw + ['top10', 'all']
+kit_options = kits + ['sw', 'mw', 'blitz', 'top10', 'all']
 uhc_submodes_ = ['uhc_duel_', 'uhc_doubles_', 'uhc_four_', 'uhc_meetup_']
 bridge_submodes_ = ['bridge_duel_', 'bridge_doubles_', 'bridge_threes_', 'bridge_four_', 'bridge_2v2v2v2_', 'bridge_3v3v3v3_', 'capture_threes_']
 min_wins_per_hour = [40, 75, 25, 80, 80, 100, 70, 20, 60, 80, 100, 20, 10, 20, 60]
@@ -67,7 +68,7 @@ gph = [70, 85, 40, 95, 85, 150, 120, 40, 75, 80, 160, 30, 12, 30, 60]
 prefix_icons_db = ['STAR', 'FANCY_STAR', 'POINTY_STAR', 'SUN', 'BIOHAZARD', 'FLOWER', 'YIN_AND_YANG', 'SNOWMAN', 'HEART', 'GG', 'SMILEY', 'DIV_RANKING', '']
 prefix_icons = ['✫', '✯', '✵', '☀', '☣', '❀', '☯', '☃', '❤', '**GG**', '^_^', '**#???**', '']
 swd_lifetime = ['poki95', 'Monk_Eez', 'MartySnoozeman', 'Lucastevo', 'D3pTTT', 'Scary_J', 'nobuh', 'mutton38', 'ImHomoAf', '1337ALBY']
-running_tasks = {}
+
 # Functions
 def dataget(ign):														# Load player's Hypixel stats
 	try:															
@@ -80,6 +81,10 @@ def dataget(ign):														# Load player's Hypixel stats
 		data = 'Failed to load data for this player.'
 	return data
 	
+def get_duels_data(data):
+	duels_data = data["player"].get("stats", {}).get("Duels", {})
+	return duels_data
+
 def mode_clean(mode):
 	while mode not in mode_list:										# check that the mode is valid
 		'''if mode == 'help':
@@ -153,6 +158,17 @@ def statusget(ign):
 		status = 'Failed to load status for this player.'
 	return status	
 
+def get_recentgames(ign):
+	try:															
+		response = requests.get(f"https://playerdb.co/api/player/minecraft/{ign}")
+		uuid = response.json()["data"]["player"]["id"]
+		url = f"https://api.hypixel.net/recentgames?key={api_key}&uuid={uuid}"
+		res = requests.get(url)
+		recentgames = res.json()
+	except KeyError:
+		recentgames = 'Failed to load recent games for this player.'
+	return recentgames
+
 def get_mode_stat(mode_db, type, duels_data):
 	if mode_db == 'all_modes':
 		return duels_data.get(type, 0)
@@ -163,10 +179,13 @@ def get_mode_stat(mode_db, type, duels_data):
 			amount += duels_data.get(submode, 0)
 		return amount
 	elif mode_db == 'bridge':
-		amount = 0
-		for submode in bridge_submodes_:
-			submode += type
-			amount += duels_data.get(submode, 0)
+		if type == 'kills' or type == 'deaths':
+			amount = duels_data.get(f'bridge_{type}', 0)
+		else:
+			amount = 0
+			for submode in bridge_submodes_:
+				submode += type
+				amount += duels_data.get(submode, 0)
 		return amount
 	elif mode_db in ['sw', 'mw', 'op']:
 		return duels_data.get(f'{mode_db}_duel_{type}', 0) + duels_data.get(f'{mode_db}_doubles_{type}', 0)
@@ -187,6 +206,30 @@ def stalk(ign):
 	map = session.get('map', False)
 	return game, mode, map
 
+def ign_based_on_discord_user(display_name, user_name):
+	ign = display_name
+	data = dataget(ign)
+	if data == 'Failed to load data for this player.':
+		ign = user_name
+	return ign
+
+def log(command, server, user):
+	return f'{command} used in {server} by {user}'
+
+def get_duels_stats(mode_db, duels_data):
+	win_count = get_mode_stat(mode_db, 'wins', duels_data)
+	win_s = "win" if win_count == 1 else "wins"
+	loss_count = get_mode_stat(mode_db, 'losses', duels_data)
+	loss_es = "loss" if loss_count == 1 else "losses"
+	kill_count = get_mode_stat(mode_db, 'kills', duels_data)
+	kill_s = "kill" if kill_count == 1 else "kills"
+	death_count = get_mode_stat(mode_db, 'deaths', duels_data)
+	death_s = "death" if death_count == 1 else "deaths"
+	return win_count, win_s, loss_count, loss_es, kill_count, kill_s, death_count, death_s
+
+def get_displayname(ign):
+	return requests.get(f"https://playerdb.co/api/player/minecraft/{ign}").json()["data"]["player"]["username"]
+
 # Bot readiness confirmation
 @bot.event
 async def on_ready():
@@ -196,7 +239,7 @@ async def on_ready():
 @bot.command(name='ping')
 async def ping(ctx, amount=1):
 	for a in range(amount):
-		await ctx.send(f'Pong! 🏓 ||{ctx.author.mention}||')
+		await ctx.reply(f'Pong! 🏓')
     
 @bot.command(name='h')
 async def h(ctx):
@@ -215,8 +258,11 @@ async def h(ctx):
 - `!kit <ign> <kit> <mode>` — Check player's kit's wins in MegaWalls, SkyWars or Blitz Duels.
   -> `!kit poki95 pyromancer sw`
 
-- `!kit <ign> <kit> <mode>` — Check player's kit's wins in MegaWalls, SkyWars or Blitz Duels.
-  -> `!kit poki95 pyromancer sw`
+- `!s <ign>` — Check player's status.
+  -> `!s poki95`
+
+- `!uuid <ign>` — Check player's UUID.
+  -> `!uuid poki95`
 
 - `!tlb <weekly/monthly>` - Check current Overall Weekly/Monthly duels leaderboard.
   -> `!tlb monthly`
@@ -228,10 +274,7 @@ For any issues, contact **`poki95`** on Discord.
 @bot.command(name='d')
 async def d(ctx, ign=None, mode='all'):
 	if ign == None:
-		ign = ctx.author.display_name
-		data = dataget(ign)
-		if data == 'Failed to load data for this player.':
-			ign = ctx.author.name
+		ign = ign_based_on_discord_user(ctx.author.display_name, ctx.author.name)
 			
 	if ign.lower() in mode_list:
 		mode = ign
@@ -246,18 +289,19 @@ async def d(ctx, ign=None, mode='all'):
 		if str(data) == "{'success': False, 'cause': 'Invalid API key'}":
 			api_key = os.getenv("HYPIXEL_API_KEY")	
 		else:
-			await ctx.send('YOU ARE A LOSER!')
+			await ctx.send('Invalid IGN.')
 		return
 
 	if mode.lower() not in mode_list:
 		await ctx.send(f'`{mode}` is not an available mode. Type `!h` for help.')
 		return
 
-	displayname = data["player"]["displayname"]
+	displayname = get_displayname(ign)
+
 	if displayname.count('_') >= 2:
 		displayname = f'`{displayname}`'
-	duels_data = data["player"].get("stats", {}).get("Duels", {})
-	mode_db = mode if mode == 'all' else mode_db_list[mode_list.index(mode.lower())]
+
+	duels_data = get_duels_data(data)
 	
 	equipped_icon = prefix_icons[prefix_icons_db.index(duels_data.get("equipped_prefix_icon", ''))]
 	equipped_color = duels_data.get("equipped_prefix_color", '')
@@ -271,18 +315,11 @@ async def d(ctx, ign=None, mode='all'):
 	wins = {}
 	messages = {}
 	
-	modes = mode_db_list if mode == 'all' else [mode_db_list[mode_list.index(mode)]]
+	modes = mode_db_list # if mode == 'all' else [mode_db_list[mode_list.index(mode)]]
 	wins_list = []
 	overall_playtime = 0
 	for mode_db in modes:
-		win_count = get_mode_stat(mode_db, 'wins', duels_data)
-		win_s = "win" if win_count == 1 else "wins"
-		loss_count = get_mode_stat(mode_db, 'losses', duels_data)
-		loss_es = "loss" if loss_count == 1 else "losses"
-		kill_count = get_mode_stat(mode_db, 'kills', duels_data)
-		kill_s = "kill" if kill_count == 1 else "kills"
-		death_count = get_mode_stat(mode_db, 'deaths', duels_data)
-		death_s = "death" if death_count == 1 else "deaths"
+		win_count, win_s, loss_count, loss_es, kill_count, kill_s, death_count, death_s = get_duels_stats(mode_db, duels_data)
 		is_oa = (mode_db == 'oa')
 		div, div_num = get_division_info(win_count, is_oa)	
 		division = f'{div} {roman.toRoman(div_num)}' if div_num != 1 and div != "None" else div
@@ -296,7 +333,7 @@ async def d(ctx, ign=None, mode='all'):
 		if mode_db != 'all_modes':			
 			overall_playtime += playtime
 			if mode_db_list_long[mode_db_list.index(mode_db)] in equipped_title:
-				current_title = f'{equipped_icon} {mode_clean} {oa_division if mode_db == '"all_modes"' else division}'
+				current_title = f'{equipped_icon} {mode_clean} {oa_division if mode_db == 'all_modes' else division}'
 			wins_list.append((win_count, f"{format_number(win_count)} {mode_clean} {win_s} - {mode_clean} {division} (~{playtime}h)"))
 
 		else:
@@ -318,21 +355,30 @@ async def d(ctx, ign=None, mode='all'):
 		message_lines.extend(msg for _, msg in sorted_modes)
 		await ctx.send('\n'.join(message_lines))
 	else:
+		mode_db = mode if mode == 'all' else mode_db_list[mode_list.index(mode.lower())]
+		win_count, win_s, loss_count, loss_es, kill_count, kill_s, death_count, death_s = get_duels_stats(mode_db, duels_data)
+		is_oa = (mode_db == 'oa')
+		div, div_num = get_division_info(win_count, is_oa)	
+		division = f'{div} {roman.toRoman(div_num)}' if div_num != 1 and div != "None" else div
+		mode_clean = mode_names[mode_db_list.index(mode_db)]
+		playtime = round((win_count + loss_count)/gph[mode_db_list.index(mode_db)-1], 1)
 		WLR = round(win_count/loss_count, 2) if loss_count != 0 else win_count
 		KDR = round(kill_count/death_count, 2) if death_count != 0 else kill_count
-		message = sub(' +', ' ', f"""✫ {mode_clean} {oa_division if mode == 'oa' else division} {get_rank(ign, data)}{displayname} (~{playtime}h)
-		{format_number(win_count)} {win_s} - {format_number(loss_count)} {loss_es} - {WLR} WLR
-		{format_number(kill_count)} {kill_s} - {format_number(death_count)} {death_s} - {KDR} KDR""")
+		if mode in ['pkd', 'tnt', 'boxing']:
+			message = sub(' +', ' ', f"""✫ {mode_clean} {oa_division if mode == 'oa' else division} {get_rank(ign, data)}{displayname} (~{playtime}h)
+			{format_number(win_count)} {win_s} - {format_number(loss_count)} {loss_es} - {WLR} WLR""")			
+		else:
+			message = sub(' +', ' ', f"""✫ {mode_clean} {oa_division if mode == 'oa' else division} {get_rank(ign, data)}{displayname} (~{playtime if mode != 'oa' else overall_playtime}h)
+			{format_number(win_count)} {win_s} - {format_number(loss_count)} {loss_es} - {WLR} WLR
+			{format_number(kill_count)} {kill_s} - {format_number(death_count)} {death_s} - {KDR} KDR""")
 		await ctx.send(message)
-	print(f'!d {ign} {mode} used in {ctx.guild.name} by {ctx.author.name}')
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))
+	# print(f'!d {ign} {mode} used in {ctx.guild.name} by {ctx.author.name}')
 	
 @bot.command(name='kit')
-async def kit(ctx, ign='None', mode='sw', kit='top10'):
-	if ign == 'None':
-		ign = ctx.author.display_name
-		data = dataget(ign)
-		if data == 'Failed to load data for this player.':
-			ign = ctx.author.name	
+async def kit(ctx, ign=None, mode='sw', kit='top10'):
+	if ign == None:
+		ign = ign_based_on_discord_user(ctx.author.display_name, ctx.author.name)
 	elif ign.lower() in kits:
 		kit = ign.lower()
 		ign = ctx.author.display_name
@@ -345,12 +391,27 @@ async def kit(ctx, ign='None', mode='sw', kit='top10'):
 		data = dataget(ign)
 		if data == 'Failed to load data for this player.':
 			ign = ctx.author.name
-	data = dataget(ign)
-	if data == 'Failed to load data for this player.':
-		await ctx.send('YOU ARE A LOSER!')
 
-	displayname = data["player"]["displayname"]
-	duels_data = data["player"].get("stats", {}).get("Duels", {})
+	data = dataget(ign)
+
+	if data == 'Failed to load data for this player.':
+		await ctx.send('Invalid IGN.')
+		return
+
+	if mode.lower() not in kit_options:
+		await ctx.send(f'`{mode}` is not an available option. Type `!h` for help.')
+		return
+	if kit.lower() not in kit_options:
+		await ctx.send(f'``{kit}` is not an available option. Type `!h` for help.')
+		return		
+	if mode.lower() in kits:
+		if kit.lower() in ['sw', 'blitz', 'mw']:
+			kit, mode = mode, kit
+		else:
+			kit, mode = mode, 'sw'
+
+	displayname = get_displayname(ign)
+	duels_data = get_duels_data(data)
 
 	if mode.lower() == 'sw':
 		sw_wins = get_mode_stat('sw', 'wins', duels_data)
@@ -464,22 +525,40 @@ async def kit(ctx, ign='None', mode='sw', kit='top10'):
 		win_s = "win" if wins == 1 else "wins"
 		message = f"{get_rank(ign, data)}{displayname} has {wins} {kit.capitalize()} {win_s}."
 		await ctx.send(message)
-	print(f'!kit {ign} {mode} {kit} used in {ctx.guild.name} by {ctx.author.name}')
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))
+	# print(f'!kit {ign} {mode} {kit} used in {ctx.guild.name} by {ctx.author.name}')
 
 @bot.command(name='s')
-async def s(ctx, ign):
+async def s(ctx, ign=None):
+	if ign == None:
+		ign = ign_based_on_discord_user(ctx.author.display_name, ctx.author.name)
 	status = statusget(ign)
 	session = status.get("session", {})
+	displayname = get_displayname(ign)
 	if session.get("online"):
 		game = session.get('gameType')
 		mode = session.get('mode')
 		map = session.get('map', False)
-		message = f"{ign}\nGame: {game}\nMode: {mode}\nMap: {map if map != False else mode}"
+		message = f"{displayname}\nGame: {game}\nMode: {mode}\nMap: {map if map != False else mode}"
 		await ctx.send(message)
 	else:
-		await ctx.send('Player offline.')
-		
-	print(f'!s {ign} used in {ctx.guild.name} by {ctx.author.name}')
+		recent_games_data = get_recentgames(ign)["games"]
+		if len(recent_games_data) != 0:
+			messages_list = []
+			for id in range(len(recent_games_data)):
+				recentgames = get_recentgames(ign)["games"][id]
+				start_time = round(recentgames.get('date', 'N/A')/1000)
+				stop_time = round(recentgames.get('ended', 'N/A')/1000)
+				game = recentgames.get('gameType')
+				mode = recentgames.get('mode')
+				map = recentgames.get('map', False)
+				messages_list.append(f"{displayname}\nStarted: <t:{start_time}:T>\nEnded: <t:{stop_time}:T>\nGame: {game}\nMode: {mode}\nMap: {map if map != False else mode}")
+			await ctx.send(f'{displayname} is (appearing) offline.\nRecent games:\n{'\n\n'.join(messages_list)}')
+		else:
+			await ctx.send(f'{displayname} is (appearing) offline. Recent games unavailable.')
+		# print(recentgames)
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))	
+	# print(f'!s {ign} used in {ctx.guild.name} by {ctx.author.name}')
 
 @bot.command(name='tlb')
 async def tlb(ctx, frequency='weekly'):
@@ -514,25 +593,28 @@ async def tlb(ctx, frequency='weekly'):
 	response = f'Duels Monthly:\n' + "\n".join(messages[m] for m in monthly_lb_data[:10])
 	await ctx.send(response)
 
-	print(f'!tlb {frequency} used in {ctx.guild.name} by {ctx.author.name}')
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))
 
 @bot.command(name='uuid')
-async def uuid(ctx, ign='poki95'):
-	await ctx.send(f"{ign}'s UUID: `{requests.get(f"https://playerdb.co/api/player/minecraft/{ign}").json()["data"]["player"]["id"]}`")
+async def uuid(ctx, ign=None):
+	if ign == None:
+		ign = ign_based_on_discord_user(ctx.author.display_name, ctx.author.name)
+	displayname = get_displayname(ign)
+	uuid = requests.get(f"https://playerdb.co/api/player/minecraft/{ign}").json()["data"]["player"]["id"]
+	await ctx.send(f"{displayname}'s UUID: `{uuid}`")
 
 @bot.command(name='kits')
 async def uuid(ctx, *, args=''):
-	await ctx.send(f'Did you mean `!kit {args}`?')
+	await ctx.send(f'Did you mean `!kit {args}`, {ctx.author.display_name}?')
 
 @bot.command(name='dodge')
 async def dodge(ctx, ign1, amount=7, ign2='poki95'):
-	if str(ctx.channel) == 'dodging':
+	if str(ctx.channel) == 'dodging' and ctx.user.name == 'poki95':
 		list1 = []
 		list2 = []
 		user_id = ctx.author.id
-		running_tasks[user_id] = True
 		x = 0
-		while x < int(amount) and running_tasks[user_id]:
+		while x < int(amount):
 			game1, mode1, map1 = stalk(ign1)
 			game2, mode2, map2 = stalk(ign2)
 			message1 = f"{mode1} on {map1 if map1 != False else game1}"
@@ -547,44 +629,31 @@ async def dodge(ctx, ign1, amount=7, ign2='poki95'):
 			await asyncio.sleep(random.randint(7, 10))
 	else:
 		await ctx.send(f'Private command.')
-		
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))	
 	print(f'Succesfully stalked {ign1} for {int(amount)} iterations')
 
-@bot.command(name='track')
-async def track(ctx, ign='poki95', amount='1', sleepstart=540, sleepstop=600):
-	if str(ctx.channel) == 'tracking':
-		data = dataget(ign)
-		duels_data = data["player"].get("stats", {}).get("Duels", {})
-		user_id = ctx.author.id
-		running_tasks[user_id] = True
-		x = 0
-		while x < int(amount) and running_tasks[user_id]:
-			wins = get_mode_stat('oa', 'wins', duels_data)
-			message = f'{ign} has {wins} wins'
-		#	if wins%100 > 90:
-			await ctx.reply(message)
-			x += 1
-			await asyncio.sleep(random.randint(sleepstart, sleepstop))
-	else:
-		await ctx.send(f'Private command.')
-		
-	print(f'Succesfully tracked {ign} for {int(amount)} iterations')
-checking = {}
+@bot.command(name='test')
+async def test(ctx, *, input):
+	if ctx.author.name == 'poki95':
+		test_commands_list = [f"{line.strip()}" for line in input.strip().split(',')]
+		for test_command in test_commands_list:
+			await ctx.send(test_command)
+			await asyncio.sleep(5)
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))
 
 @bot.command(name='session')
 async def session(ctx, ign='poki95'):
-	if str(ctx.channel) == 'tracking':
+	if str(ctx.channel) == 'tracking' and ctx.user.name == 'poki95':
 		data = dataget(ign)
-		duels_data = data["player"].get("stats", {}).get("Duels", {})
+		duels_data = get_duels_data(data)
 		user_id = ctx.author.id
-		running_tasks[user_id] = True
 		start_wins = get_mode_stat('all_modes', 'wins', duels_data)
 		start_losses = get_mode_stat('all_modes', 'losses', duels_data)
 		await ctx.send(f'current wins: {start_wins}')
-		while running_tasks[user_id]:
+		while True:
 			await asyncio.sleep(300)
 			data = dataget(ign)
-			duels_data = data["player"].get("stats", {}).get("Duels", {})
+			duels_data = get_duels_data(data)
 			wins = get_mode_stat('all_modes', 'wins', duels_data)
 			losses = get_mode_stat('all_modes', 'losses', duels_data)
 			if losses == start_losses:
@@ -599,11 +668,12 @@ async def session(ctx, ign='poki95'):
 @bot.command(name='check')
 async def check(ctx, ign='None', type='wins', mode='all_modes'):
 	data = dataget(ign)
-	duels_data = data["player"].get("stats", {}).get("Duels", {})
+	duels_data = get_duels_data(data)
 	stat = get_mode_stat(mode, type, duels_data)
 	await ctx.send(f'{ign} has {stat} {type} in {mode}')
+	print(log(ctx.message.content, ctx.guild.name, ctx.author.name))
 
-@bot.command(name='stop')
+'''@bot.command(name='stop')
 async def stop(ctx):
 	if ctx.author.name in ['poki95', 'awesomeduy']:
 		user_id = ctx.author.id
@@ -611,6 +681,8 @@ async def stop(ctx):
 			running_tasks[user_id]
 			await ctx.reply(f"Succesfully stopped {ctx.author.name}'s running task.")
 		else:
-			await ctx.reply(f"No tasks to be stopped for {ctx.author.name}.")
-# Run the bot!kit 
+			await ctx.reply(f"No tasks to be stopped for {ctx.author.name}.")'''
+
+
+# Run the bot 
 bot.run(TOKEN)
